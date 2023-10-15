1338322cd3b806b028c4e7b6f3ddbd4ae0c7834d
Mattias Lasersköld <mattias.l.sk@gmail.com>
2023-10-15 13:41:49 +0200

    Some documentation

--- include/fast-json/jsonnode.h
+++ include/fast-json/jsonnode.h
@@ -14,6 +14,8 @@ concept HasFromJsonFunction = requires(T &obj, const class JsonNode &json) {
     { obj.from_json(json) };
 };
 
+/// Object containing parsed json information. Note that you need to use
+/// json::parse_json() to get one of those objects
 class JsonNode {
 public:
     class const_iterator {
@@ -70,14 +72,17 @@ public:
     JsonNode(const Token &value)
         : _value(value) {}
 
+    /// Return the raw token that represents the value
     const Token &value() const {
         return _value;
     }
 
+    /// Set the token
     const Token &value(Token value) {
         return _value = value;
     }
 
+    /// The first child
     const JsonNode &front() const {
         if (!_children) {
             throw std::out_of_range{"no children in node"};
@@ -95,6 +100,9 @@ public:
     const_iterator end() const {
         return const_iterator(nullptr);
     }
+
+    /// Returns the first of the children. The next child is obtained by calling
+    /// next() on the item
     const JsonNode *children() const {
         return _children;
     }
@@ -111,6 +119,7 @@ public:
         _next = node;
     }
 
+    /// Get child by name
     const JsonNode &at(std::string_view name) const {
         if (_value.type == TokenType::KEY) {
             return _children->at(name);
@@ -126,10 +135,13 @@ public:
         throw std::out_of_range{"element " + std::string{name} + " not found"};
     }
 
+    /// Alias for at. Note that the function is const, so no element can be
+    /// added with this function
     const JsonNode &operator[](std::string_view name) const {
         return at(name);
     }
 
+    /// Try to find a element by name, but return null if no child was found
     const JsonNode *find(std::string_view name) const {
         if (_value.type == TokenType::KEY) {
             return _children->find(name);
@@ -144,9 +156,9 @@ public:
         return nullptr;
     }
 
-    // Iterato through an object and call a function for each key-value-pair
-    // The first argument should be the name and the second a const
-    // JsonNode&
+    /// Iterato through an object and call a function for each key-value-pair
+    /// The first argument should be the name and the second a const
+    /// JsonNode&
     template <typename F>
     void visit(F f) const {
         validate_object();
@@ -159,10 +171,11 @@ public:
         }
     }
 
+    /// Get the value as a number as a number.
     template <typename T = int>
     T number() const {
         validate_number();
-        T out = {};
+        auto out = T{};
         const std::from_chars_result result =
             std::from_chars(_value.value.data(),
                             _value.value.data() + _value.value.size(),
@@ -179,12 +192,14 @@ public:
         return _value.value == "true";
     }
 
+    /// Get the raw string content as seen in the string that was parsed to
+    /// generate the json object
     std::string_view raw() const {
         validate_string();
         return _value.value;
     }
 
-    // Get a string where the escape characters is applied
+    /// Get a string where the escape characters is applied
     std::string str() const {
         auto res = std::string{};
 
@@ -243,6 +258,9 @@ public:
         return res;
     }
 
+    /// Convert the value to a custuom type.
+    /// There is template specialization for objects that has a member function
+    /// with the signature `from_json(const JsonNode&)`
     template <typename T>
     T as() const {
         return number<T>();
@@ -256,6 +274,7 @@ public:
         return ret;
     }
 
+    /// The same as `as<T>` but places the value on a specified variable
     template <typename T>
     void get_to(T &value) const {
         value = as<T>();
@@ -316,6 +335,9 @@ bool JsonNode::as<bool>() const {
     return boolean();
 }
 
+/// Print the object to a output stream
+/// Note that the JsonNode is not commonly used for output (since there is the
+/// JsonOut object), but this could be used when you want to view incoming data
 inline void dump(const JsonNode &node, std::ostream &os, int indent = 0) {
 
     int current_width =
