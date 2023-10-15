4bc0b136142dcf59f6b0254ef30074ad52176be0
Mattias Lasersköld <mattias.l.sk@gmail.com>
2023-10-15 14:15:53 +0200

    More number of macro implementations

--- include/fast-json/serialize.h
+++ include/fast-json/serialize.h
@@ -85,7 +85,117 @@
         json[#X3].get_to(X3);                                                  \
         json[#X4].get_to(X4);                                                  \
     }
-// Add more _FAST_JSON_SERIALIZE_INTRUSIVE_N for N=4,6,8,10,...
+
+#define _FAST_JSON_SERIALIZE_INTRUSIVE_6(X0, X1, X2, X3, X4, X5)               \
+    void to_json(json::JsonOut &json) const {                                  \
+        json[#X0] = X0;                                                        \
+        json[#X1] = X1;                                                        \
+        json[#X2] = X2;                                                        \
+        json[#X3] = X3;                                                        \
+        json[#X4] = X4;                                                        \
+        json[#X5] = X5;                                                        \
+    }                                                                          \
+    void from_json(const json::JsonNode &json) {                               \
+        json[#X0].get_to(X0);                                                  \
+        json[#X1].get_to(X1);                                                  \
+        json[#X2].get_to(X2);                                                  \
+        json[#X3].get_to(X3);                                                  \
+        json[#X4].get_to(X4);                                                  \
+        json[#X5].get_to(X5);                                                  \
+    }
+
+#define _FAST_JSON_SERIALIZE_INTRUSIVE_7(X0, X1, X2, X3, X4, X5, X6)           \
+    void to_json(json::JsonOut &json) const {                                  \
+        json[#X0] = X0;                                                        \
+        json[#X1] = X1;                                                        \
+        json[#X2] = X2;                                                        \
+        json[#X3] = X3;                                                        \
+        json[#X4] = X4;                                                        \
+        json[#X5] = X5;                                                        \
+        json[#X6] = X6;                                                        \
+    }                                                                          \
+    void from_json(const json::JsonNode &json) {                               \
+        json[#X0].get_to(X0);                                                  \
+        json[#X1].get_to(X1);                                                  \
+        json[#X2].get_to(X2);                                                  \
+        json[#X3].get_to(X3);                                                  \
+        json[#X4].get_to(X4);                                                  \
+        json[#X5].get_to(X5);                                                  \
+        json[#X6].get_to(X6);                                                  \
+    }
+
+#define _FAST_JSON_SERIALIZE_INTRUSIVE_8(X0, X1, X2, X3, X4, X5, X6, X7)       \
+    void to_json(json::JsonOut &json) const {                                  \
+        json[#X0] = X0;                                                        \
+        json[#X1] = X1;                                                        \
+        json[#X2] = X2;                                                        \
+        json[#X3] = X3;                                                        \
+        json[#X4] = X4;                                                        \
+        json[#X5] = X5;                                                        \
+        json[#X6] = X6;                                                        \
+        json[#X7] = X7;                                                        \
+    }                                                                          \
+    void from_json(const json::JsonNode &json) {                               \
+        json[#X0].get_to(X0);                                                  \
+        json[#X1].get_to(X1);                                                  \
+        json[#X2].get_to(X2);                                                  \
+        json[#X3].get_to(X3);                                                  \
+        json[#X4].get_to(X4);                                                  \
+        json[#X5].get_to(X5);                                                  \
+        json[#X6].get_to(X6);                                                  \
+        json[#X7].get_to(X7);                                                  \
+    }
+
+#define _FAST_JSON_SERIALIZE_INTRUSIVE_9(X0, X1, X2, X3, X4, X5, X6, X7, X8)   \
+    void to_json(json::JsonOut &json) const {                                  \
+        json[#X0] = X0;                                                        \
+        json[#X1] = X1;                                                        \
+        json[#X2] = X2;                                                        \
+        json[#X3] = X3;                                                        \
+        json[#X4] = X4;                                                        \
+        json[#X5] = X5;                                                        \
+        json[#X6] = X6;                                                        \
+        json[#X7] = X7;                                                        \
+        json[#X8] = X8;                                                        \
+    }                                                                          \
+    void from_json(const json::JsonNode &json) {                               \
+        json[#X0].get_to(X0);                                                  \
+        json[#X1].get_to(X1);                                                  \
+        json[#X2].get_to(X2);                                                  \
+        json[#X3].get_to(X3);                                                  \
+        json[#X4].get_to(X4);                                                  \
+        json[#X5].get_to(X5);                                                  \
+        json[#X6].get_to(X6);                                                  \
+        json[#X7].get_to(X7);                                                  \
+        json[#X8].get_to(X8);                                                  \
+    }
+
+#define _FAST_JSON_SERIALIZE_INTRUSIVE_10(                                     \
+    X0, X1, X2, X3, X4, X5, X6, X7, X8, X9)                                    \
+    void to_json(json::JsonOut &json) const {                                  \
+        json[#X0] = X0;                                                        \
+        json[#X1] = X1;                                                        \
+        json[#X2] = X2;                                                        \
+        json[#X3] = X3;                                                        \
+        json[#X4] = X4;                                                        \
+        json[#X5] = X5;                                                        \
+        json[#X6] = X6;                                                        \
+        json[#X7] = X7;                                                        \
+        json[#X8] = X8;                                                        \
+        json[#X9] = X9;                                                        \
+    }                                                                          \
+    void from_json(const json::JsonNode &json) {                               \
+        json[#X0].get_to(X0);                                                  \
+        json[#X1].get_to(X1);                                                  \
+        json[#X2].get_to(X2);                                                  \
+        json[#X3].get_to(X3);                                                  \
+        json[#X4].get_to(X4);                                                  \
+        json[#X5].get_to(X5);                                                  \
+        json[#X6].get_to(X6);                                                  \
+        json[#X7].get_to(X7);                                                  \
+        json[#X8].get_to(X8);                                                  \
+        json[#X9].get_to(X9);                                                  \
+    }
 
 // Main macro that selects the appropriate implementation based on argument
 // count
